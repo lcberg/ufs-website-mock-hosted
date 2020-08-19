b5d5cfce03a50ef084ed57af467783bbac3e54b1
lcberg <50460080+lcberg@users.noreply.github.com>
2020-08-19 17:32:16 +0200

    demo with blog

--- src/views/blog/blogPage.vue
+++ src/views/blog/blogPage.vue
@@ -1,5 +1,36 @@
 <template>
-  <h2>blogpage</h2>
+  <div class="container mx-auto flex flex-col my-24">
+    <div class="blogpost flex flex-row items-center text-gray-800">
+      <div class="blogname flex-grow text-left">
+        <p>by Louis Berger - Informationationist</p>
+        <h2 class="text-4xl w-2/3"> WHY I DID NOT THINK OF BETTER MOCK CONTENT - HAIKU</h2>
+      </div>
+      <div class="blog-image w-1/5">
+        <img :src="require('../../assets/blog/blogone.jpg')">
+      </div>
+    </div>
+
+    <div class="blogpost flex flex-row mt-24 items-center text-gray-800">
+      <div class="blogname flex-grow text-left">
+        <p>by André Berger - CEO</p>
+        <h2 class="text-4xl w-2/3">THE DIGITAL AGE WILL BE LED WITH PENCILS</h2>
+      </div>
+      <div class="blog-image w-1/5">
+        <img :src="require('../../assets/blog/blogtwo.jpg')">
+      </div>
+    </div>
+
+    <div class="blogpost flex flex-row mt-24 items-center text-gray-800">
+      <div class="blogname flex-grow text-left">
+        <p>by Louis Berger - Informationationist</p>
+        <h2 class="text-4xl w-2/3"> THIS WEBSITE IS A MOCKUP AND I EVEN LEARNED SOMETHING FROM IT</h2>
+      </div>
+      <div class="blog-image w-1/5">
+        <img :src="require('../../assets/blog/blogthree.jpg')">
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +39,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .blogpost {
+    border-top: 1px solid grey;
+    border-bottom: 1px solid grey;
+    padding-top: 15px;
+    padding-bottom: 15px;
 
+    &:hover {
+      background-color: darken(white, 40%);
+      cursor: pointer;
+    }
+  }
+
+  .blogname {
+    max-width: 80%;
+  }
 </style>
